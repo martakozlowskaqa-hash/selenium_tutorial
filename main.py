d54521225c42e320cee27f411bfc73d0e5ada5ac
martakozlowskaqa-hash <martakozlowska.qa@gmail.com>
2026-02-21 15:45:16 +0100

    Create automated test of Sign in - v1

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from selenium.webdriver import Chrome
 from time import sleep
+from selenium.webdriver.common.by import By
+
+class Gender:
+    MALE = 0
+    FEMALE = 1
+
+#test data
+DATA_EMAIL = "janedoe@sample.sample"
+DATA_GENDER = Gender.MALE
 
 driver = Chrome()
 # open landing page
-driver.get("https://www.kozminski.edu.pl/pl")
-sleep(5)
+driver.get("https://automationpractice.techwithjatin.com/")
+sleep(3)
+
 # maximize window
 driver.maximize_window()
-driver.switch_to_alert()
-element.click("")
+
+#find element
+sign_in_a = driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
+sign_in_a.click()
+
+#click email input
+driver.find_element(By.ID, "email_create").send_keys(DATA_EMAIL)
+
+#click create account button
+driver.find_element(By.ID, "SubmitCreate").click()
+
+driver.implicitly_wait(5)
+#select gender
+if DATA_GENDER == Gender.FEMALE:
+    driver.find_element(By.XPATH, '//label[@for="id_gender2"]').click()
+else:
+    driver.find_element(By.XPATH, '//label[@for="id_gender1"]').click()
+
+print(type(sign_in_a))
+sleep(5)
 driver.quit()
